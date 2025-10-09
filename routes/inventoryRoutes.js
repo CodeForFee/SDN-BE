e929db6f1302ead7ce6f2559e7e926def1e3751a
fsa_HaiLH <hailhse183904@fpt.edu.vn>
2025-10-09 13:25:32 +0700

    Add inventoryController and quoteController

--- routes/inventoryRoutes.js
+++ routes/inventoryRoutes.js
@@ -1,15 +1,35 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const inventoryController = require('../controllers/inventoryController');
-const { protect } = require('../middleware/authMiddleware');
-const { allowRoles } = require('../middleware/roles');
+const inventoryController = require("../controllers/inventoryController");
+const { protect } = require("../middleware/authMiddleware");
+const { authorizeRoles } = require("../middleware/roles");
 
 // Xem tồn kho
-// router.get('/', protect, allowRoles('Dealer Manager', 'EVM Staff', 'Admin'), inventoryController.getInventory);
+router.get(
+  "/",
+  protect,
+  authorizeRoles("Dealer Manager", "EVM Staff", "Admin"),
+  inventoryController.getInventory
+);
 
 // Quản lý tồn kho chỉ EVM Staff & Admin
-// router.post('/', protect, allowRoles('EVM Staff', 'Admin'), inventoryController.createInventory);
-// router.put('/:id', protect, allowRoles('EVM Staff', 'Admin'), inventoryController.updateInventory);
-// router.delete('/:id', protect, allowRoles('Admin'), inventoryController.deleteInventory);
+router.post(
+  "/",
+  protect,
+  authorizeRoles("EVM Staff", "Admin"),
+  inventoryController.createInventory
+);
+router.put(
+  "/:id",
+  protect,
+  authorizeRoles("EVM Staff", "Admin"),
+  inventoryController.updateInventory
+);
+router.delete(
+  "/:id",
+  protect,
+  authorizeRoles("Admin"),
+  inventoryController.deleteInventory
+);
 
 module.exports = router;
